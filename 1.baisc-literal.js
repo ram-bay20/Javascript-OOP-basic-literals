e193df710e81adc93b1585eb6099f484c2b7b2c8
Ramnath Mandal <107981223+ram-bay20@users.noreply.github.com>
2022-07-25 18:57:16 +0545

    Add files via upload

---
+++ 1.baisc-literal.js
@@ -0,0 +1,39 @@
+//const s1 = 'ramnath';
+//console.log(s1.toUpperCase());
+//console.log(typeof s1);
+
+//const s2 = new String('mandal');
+//console.log(s2.toUpperCase());
+//console.log(typeof s2);
+
+//console.log(window);
+//alert('hello');
+
+//console.log(navigator.appVersion);
+
+
+const book1 = {
+     title : 'Harry Potter',
+     author : 'J.K Rowling',
+     year : '1994.',
+     summary : function(){
+          return `${this.title} was written by ${this.author} in ${this.year}`;
+     }
+}
+
+//console.log(book1.summary());
+//console.log(Object.values(book1));
+//console.log(Object.keys(book1));
+
+const book2 = {
+     title : 'Genaeral Theory of Relativity',
+     author : 'Albert Einstein',
+     year : '1905.',
+     summary : function(){
+          return `${this.title} was written by ${this.author} in ${this.year}`;
+     }
+}
+
+//console.log(book2.summary());
+//console.log(Object.values(book2));
+//console.log(Object.keys(book2));
